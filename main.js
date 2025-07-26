3af030458772b45eb1930e9085f54f26f9664e30
fabwhite <Ecrireafabien@gmail.com>
2025-07-26 15:45:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+import { db } from "./firebase-config.js";
+import {
+  collection,
+  addDoc,
+  onSnapshot,
+  serverTimestamp,
+  query,
+  orderBy,
+  where,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+let userNumber = "";
+const chatDiv = document.getElementById("chat");
+const greeting = document.getElementById("greeting");
+const messagesDiv = document.getElementById("messages");
+const notification = document.getElementById("notification");
+
+window.login = () => {
+  const input = document.getElementById("user-number");
+  userNumber = input.val
 // Chat avec Firebase et signalement
